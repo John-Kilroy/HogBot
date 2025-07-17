e34d13c654e29ee494942dea6a22297206d576b5
John Kilroy <55547547+John-Kilroy@users.noreply.github.com>
2025-07-16 20:18:42 -0400

    Update hog.js

--- commands/hog.js
+++ commands/hog.js
@@ -6,7 +6,17 @@ module.exports = {
             "Did someone say hog?",
             "You called?",
             "Looking for me handsome?",
-            "I'm listening..."
+            "I'm listening...",
+            "Don't go bacon my heart!",
+            "Let's bring home the bacon",
+            "I'm hog-wild for this idea",
+            "Stop hamming it up!",
+            "Sow what?",
+            "You're really hogging the spotlight",
+            "You're swine and dandy",
+            "Sure, that'll happen when pigs fly!",
+            "You're porktunate to know me",
+            "Hog to meet you"
         ];
         const reply = replies[Math.floor(Math.random() * replies.length)];
         return msg.channel.send(reply);
